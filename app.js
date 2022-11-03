f0a30bcf275d32954fb043cc934599d9bd058bf3
Devin Withoft <teddyfest@gmail.com>
2022-11-03 15:09:48 -0700

    Fight functionality works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const heroes = [
     {
-        name: 'sloth-sparticus',
+        name: 'sloth',
         type: 'duck',
         emoji: '🦆',
         damage: 5,
@@ -28,20 +28,55 @@ const boss = {
 
 
 
+function attackBoss() {
+    let damage = 0
+    heroes.forEach(h => { 
+        damage += h.damage
+    })
+    boss.health -= damage
+    if (boss.health <= 0) boss.health = 0
+    console.log(boss.health);
+    drawHealth()
+}
+
+function bossAttack() {
+    let bossAttacking = setInterval(() => {
+        if (boss.health == 0) {
+            clearInterval(bossAttacking)
+            console.log("boss dead")
+        } else {
+            heroes.forEach(hh => {
+                hh.health -= 5
+            })}drawHealth()
+        }, 1000)
+}
 
 
+function drawHealth() {
+    let bossHealth = document.getElementById('squee')
+    bossHealth.querySelector(".progress-bar").style.width = boss.health + '%'
+    heroes.forEach(hh => {
+        document.getElementById(hh.name).querySelector(".health").innerText = hh.health
+        if (hh.health <=0 ) {hh.health = 0}
+    }
+        )
+    }
 
 
+function startFight() {
+    if (boss.health < boss.maxHealth) {
+        bossAttack()
+        clearInterval(firstStrike)} 
+}
 
+function endFight() {
+    if (boss.health == 0) {
+        clearInterval(bossAttacking),
+            console.log(bossAttacking)
+    }
+}
 
-
-
-
-
-
-
-
-
+let firstStrike = setInterval(() => { startFight() }, 500)
 
 
 
@@ -67,6 +102,4 @@ const boss = {
     
 // }, interval);
 // clearInterval()
-
-
 
