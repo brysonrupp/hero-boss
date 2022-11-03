619df1bc39f3b86e8202c07b42457943f2813d27
bryson rupp <brysonrupp7@gmail.com>
2022-11-03 14:18:17 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const heroes = [
+    {
+        name: 'sloth-sparticus',
+        type: 'duck',
+        emoji: '🦆',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'phaelius',
+        type: 'elf',
+        emoji: '🧝‍♂️',
+        damage: 10,
+        health: 50
+    }
+]
+
+
+const boss = {
+    name:'squee',
+    emoji: '👺',
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ANCHOR - notes 
+// function do something() {
+// }
+
+// let doSomeThingElse = function () {
+// }
+
+// let thing 
+// thing = () => {
+// }
+// setTimeout(() => (console.log('done')), seconds * 1000) {
+    
+// }
+
+// setInterval(() => {
+    
+// }, interval);
+// clearInterval()
+
+
+
